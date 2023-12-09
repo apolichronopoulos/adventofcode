7afbae737a311b4b3f71c6f17bc2eb70a0b52f07
alexandros.polichronopoulos <alexandros.polichronopoulos@d-one.ai>
2023-12-09 11:11:55 +0200

    init day 9 - part 2

--- python/puzzle_2023_09.py
+++ python/puzzle_2023_09.py
@@ -1,6 +1,5 @@
 from timeit import default_timer as timer
 from datetime import datetime
-import numpy as np
 
 elements = []
 max_size = 0
@@ -22,7 +21,7 @@ def read_file(filename, part=1):
             max_size = len(x)
 
 
-def solve_brute_force(part=1):
+def solve(part=1):
     res = 0
     for i in range(0, len(elements)):
         history = []
@@ -39,20 +38,25 @@ def solve_brute_force(part=1):
             history.append(next_line)
             line += 1
 
-        while len(history[0]) < max_size + 1:
-            current_size = len(history)
-            num = 0
-            for x in range(current_size - 1, -1, -1):
-                num += (history[x])[len(history[x]) - 1]
-                history[x].append(num)
+        if part == 2:
+            while len(history[0]) < max_size + 1:
+                current_size = len(history)
+                num = 0
+                for x in range(current_size - 1, -1, -1):
+                    num = history[x][0] - num
+                    new_line = [num]
+                    new_line.extend(history[x])
+                    history[x] = new_line
+        else:
+            while len(history[0]) < max_size + 1:
+                current_size = len(history)
+                num = 0
+                for x in range(current_size - 1, -1, -1):
+                    num += history[x][len(history[x]) - 1]
+                    history[x].append(num)
 
+        print(history)
         res += num
-
-    print(f"res: {res}")
-
-
-def solve_smart(part=1):
-    res = 0
 
     print(f"res: {res}")
 
@@ -61,19 +65,16 @@ def puzzle1(filename):
     start = timer()
     print(f"\n\npuzzle1: {filename}")
     read_file(filename)
-    solve_brute_force()
+    solve()
     end = timer()
     print(f"Time elapsed (in seconds): {end - start}")
 
 
-def puzzle2(filename, brute=True):
+def puzzle2(filename):
     start = timer()
     print(f"\n\npuzzle2: {filename}")
     read_file(filename, 2)
-    if brute:
-        solve_brute_force(2)
-    else:
-        solve_smart(2)
+    solve(2)
     end = timer()
     print(f"Time elapsed (in seconds): {end - start}")
 
@@ -85,9 +86,8 @@ if __name__ == '__main__':
     print("Start Time =", current_time)
     # puzzle1('../puzzles/2023/09/example.txt')  # result -> 114
     # puzzle1('../puzzles/2023/09/input.txt')  # result -> 1584748274
-    puzzle2('../puzzles/2023/09/example2.txt')  # result ->
-    # puzzle2('../puzzles/2023/09/example2.txt', False)  # result ->
-    # puzzle2('../puzzles/2023/09/input.txt', False)  # result ->
+    # puzzle2('../puzzles/2023/09/example.txt')  # result -> 2
+    puzzle2('../puzzles/2023/09/input.txt')  # result -> 1026
 
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
