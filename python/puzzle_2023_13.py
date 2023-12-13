a34591612259cff41cdb8c5d05b4847ef62ec65e
alexandros.polichronopoulos <alexandros.polichronopoulos@d-one.ai>
2023-12-14 01:37:43 +0200

    init day 13 part 1

--- python/puzzle_2023_13.py
+++ python/puzzle_2023_13.py
@@ -23,7 +23,6 @@ def read_file(filename, part=1):
             temp_gears = []
             continue
         if part == 2:
-            # g = "?".join(5 * [line[0]])
             temp_gears.append(line)
         temp_gears.append(line)
     if temp_gears:
@@ -31,36 +30,35 @@ def read_file(filename, part=1):
 
 
 def solve():
-    # print_index(gears, "\n")
     res = 0
     for c, case in enumerate(gears):
-        res_rows = check_symmetric_in_rows(case.copy())
-        res_cols = check_symmetric_in_cols(case.copy())
-        print(f"---------> res_cols: {res_cols} <---------")
-        print(f"---------> res_rows: {res_rows} <---------")
+        print(f"{c} case")
+        res_rows = check_symmetric_in_rows(case)
+        res_cols = check_symmetric_in_cols(case)
+        if res_rows:
+            print(f"---------> res_rows: {res_rows} <---------")
+        if res_cols:
+            print(f"---------> res_cols: {res_cols} <---------")
         res += ((res_rows * 100) + res_cols)
     print(f"---------> final result: {res} <---------")
     return res
 
 
-def check_symmetric_in_rows(rows):
-    size = len(rows) // 2
-    odd = len(rows) % 2 == 1
-    if odd:
-        c1 = check_symmetric_in_rows(rows.copy()[:-1])
-        c2 = check_symmetric_in_rows(rows.copy()[1:])
-        return c1 + (c2 + 1 if c2 else 0)
-    else:
-        for i in range(0, size):
-            s1 = i
-            s2 = len(rows) - i - 1
-            if rows[s1] != rows[s2]:
-                return 0
-        return size
+def check_symmetric_in_rows(rows, index=1):
+    if len(rows) // 2 < 1 or index > len(rows) - 1:
+        return 0
+    start = max(0, 2 * index - len(rows))
+    for i in range(start, index):
+        s1 = i
+        s2 = 2 * index - s1 - 1
+        r1 = rows[s1]
+        r2 = rows[s2]
+        if r1 != r2:
+            return check_symmetric_in_rows(rows, index + 1)
+    return index
 
 
-def check_symmetric_in_cols(rows):
-    print(f"-------------")
+def check_symmetric_in_cols(rows, index=1):
     num_cols = len(rows[0])
 
     cols = num_cols * ['']
@@ -68,8 +66,9 @@ def check_symmetric_in_cols(rows):
         for j, c in enumerate(row):
             cols[j] += c
 
-    print_index(cols)
-    return check_symmetric_in_rows(cols)
+    # print_index(cols)
+    # print(f"-------------")
+    return check_symmetric_in_rows(cols, index)
 
 
 def puzzle1(filename):
@@ -97,11 +96,11 @@ if __name__ == '__main__':
     print("Start Time =", current_time)
 
     # puzzle1('../puzzles/2023/13/example.txt')  # result -> 405
-    # puzzle1('../puzzles/2023/13/example2.txt')  # result -> 700?
-    puzzle1('../puzzles/2023/13/input.txt')  # result -> 716 is not correct
-    # puzzle1('../puzzles/2023/13/input.txt')  # result -> 2849 That's not the right answer; your answer is too low.
-    # puzzle2('../puzzles/2023/13/example.txt')  # result -> result 6
-    # puzzle2('../puzzles/2023/13/input.txt')  # result -> 4546215031609 but won't run // sigkill error
+    # puzzle1('../puzzles/2023/13/example2.txt')  # result -> 1200
+    # puzzle1('../puzzles/2023/13/example3.txt')  # result -> 709
+    puzzle1('../puzzles/2023/13/input.txt')  # result -> 30802 correct
+    # puzzle2('../puzzles/2023/13/example.txt')  # result -> ?
+    # puzzle2('../puzzles/2023/13/input.txt')  # result -> ?
 
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
