97a552d89bbb6988fd5d5f212f4ff60f68b04c9a
alexandros.polichronopoulos <alexandros.polichronopoulos@d-one.ai>
2023-12-14 15:59:36 +0200

    init day 14 part 2

--- python/puzzle_2023_14.py
+++ python/puzzle_2023_14.py
@@ -1,14 +1,9 @@
 from datetime import datetime
-from functools import lru_cache
 from timeit import default_timer as timer
 
-from utils.utils import print_index, split_into_tokens, replace_char, print_color, set_print_color, \
-    reset_print_color
+from colorama import Fore, Back, init
 
-from numpy import *
-
-from colorama import Fore, Back, Style, init
-from termcolor import colored
+from utils.utils import print_index, replace_char, print_color
 
 platform = []
 
@@ -24,18 +19,69 @@ def read_file(filename, part=1):
 
 
 def solve(part=1):
-    print_index(platform, color=Fore.CYAN)
+    print_index(platform, color=Fore.CYAN, ending="")
+    # original_platform = platform.copy()
+
     res = 0
+    if part == 1:
+        move_north(platform)
+        print_index(platform, color=Fore.RED, ending="")
+        res = calculate_damage(platform)
+    else:
+        cycles = 1000000000
+        # cycles = 3
+        # cycles = 1000
+        results = []
+        repeating = []
+        prefix_numbers = []
+        for cycle in range(1, cycles + 1):
+            print(f"cycle: {cycle}")
+            move_north(platform)
+            move_west(platform)
+            move_south(platform)
+            move_east(platform)
+            results.append(str(calculate_damage(platform)))
 
-    original_platform = platform.copy()
-    move_north(platform)
-    res = calculate_damage(platform)
+            # prework = 2 steps
+            # cyclic repeat loop  every 7 steps
+            # '87,69,69,69,65,64,65,63,68,69,69,65,64,65,63,68,69,69,65,64,65,63,68,69,69,65,64,65,63,68'
+            #        69,69,65,64,65,63,68
 
+            found_cycle = False
+            cycle_numbers = []
+            for cycle_size in range(3, len(results) // 2):
+                if found_cycle:
+                    break
+                counter = 1
+                last_numbers = results[-cycle_size:]
+                previous_numbers = results[:-cycle_size]
+                while len(previous_numbers) >= cycle_size:
+                    last_numbers2 = previous_numbers[-cycle_size:]
+                    previous_numbers2 = previous_numbers[:-cycle_size]
+                    if last_numbers == last_numbers2:
+                        counter += 1
+                        previous_numbers = previous_numbers2
+                        if counter >= 4:
+                            found_cycle = True
+                            cycle_numbers.extend(last_numbers)
+                            break
+                    else:
+                        break
+
+            if found_cycle:
+                print(f"found_cycle: {cycle_numbers}")
+                repeating.extend(cycle_numbers)
+                prefix_numbers = results[:-4 * len(cycle_numbers)]
+                break
+        if repeating:
+            required = cycles - len(prefix_numbers)
+            target = required % len(repeating)
+            res = repeating[target - 1]
     print_color(f"---------> final result: {res} <---------", Fore.LIGHTRED_EX, Back.LIGHTYELLOW_EX)
     return res
 
 
-# ↑ north / ← west / → east / ↓ south
+# / ↑ north / ← west / ↓ south / → east /
 def move_north(platform, index=0):
     if index == len(platform) - 1:
         return
@@ -51,6 +97,53 @@ def move_north(platform, index=0):
     return move_north(platform, index + 1)
 
 
+# / ↑ north / ← west / ↓ south / → east /
+def move_west(platform, index=0):
+    if index == len(platform[0]) - 1:
+        return
+    for j in range(0, len(platform[0])):
+        if j == 0:
+            continue
+        next_j = j - 1
+        for i, row in enumerate(platform):
+            stone = platform[i][j]
+            if stone == 'O':
+                if platform[i][next_j] == '.':
+                    platform[i] = replace_char(platform[i], stone, next_j)
+                    platform[i] = replace_char(platform[i], '.', j)
+    return move_west(platform, index + 1)
+
+
+# / ↑ north / ← west / ↓ south / → east /
+def move_south(platform, index=0):
+    if index == len(platform) - 1:
+        return
+    for i in range(len(platform) - 2, -1, -1):
+        next_i = i + 1
+        for j, column in enumerate(platform[i]):
+            stone = platform[i][j]
+            if stone == 'O':
+                if platform[next_i][j] == '.':
+                    platform[next_i] = replace_char(platform[next_i], stone, j)
+                    platform[i] = replace_char(platform[i], '.', j)
+    return move_south(platform, index + 1)
+
+
+# / ↑ north / ← west / ↓ south / → east /
+def move_east(platform, index=0):
+    if index == len(platform[0]) - 1:
+        return
+    for j in range(len(platform[0]) - 2, -1, -1):
+        next_j = j + 1
+        for i, row in enumerate(platform):
+            stone = platform[i][j]
+            if stone == 'O':
+                if platform[i][next_j] == '.':
+                    platform[i] = replace_char(platform[i], stone, next_j)
+                    platform[i] = replace_char(platform[i], '.', j)
+    return move_east(platform, index + 1)
+
+
 def calculate_damage(platform):
     damage = 0
     for i, row in enumerate(platform):
@@ -59,6 +152,24 @@ def calculate_damage(platform):
             if stone == 'O':
                 damage += len(platform) - i
     return damage
+
+
+# / ↑ north / ← west / ↓ south / → east /
+@DeprecationWarning
+def rotate_platform(platform, index=0, clockwise=True):
+    num_cols = len(platform[0])
+    rotated = num_cols * ['']
+    for i, row in enumerate(platform):
+        for j, c in enumerate(row):
+            rotated[j] = c + rotated[j]
+
+    platform.clear()
+    platform.extend(rotated)
+
+    if index == 0:
+        return platform
+    else:
+        return rotate_platform(platform, index - 1)
 
 
 def puzzle1(filename):
@@ -87,9 +198,9 @@ if __name__ == '__main__':
     print_color(f"Start Time = {current_time}", Fore.YELLOW)
 
     # puzzle1('../puzzles/2023/14/example.txt')  # result -> 136
-    puzzle1('../puzzles/2023/14/input.txt')  # result -> 108955
-    # puzzle2('../puzzles/2023/14/example.txt')  # result ->
-    # puzzle2('../puzzles/2023/14/input.txt')  # result ->
+    # puzzle1('../puzzles/2023/14/input.txt')  # result -> 108955
+    # puzzle2('../puzzles/2023/14/example.txt')  # result -> 64 ? 1000000000 cycles
+    puzzle2('../puzzles/2023/14/input.txt')  # result -> 106689 ? 1000000000 cycles
 
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
