9013c2ef2d0ba7cf2477b97dbe55dd9e67699f6a
alexandros.polichronopoulos <alexandros.polichronopoulos@d-one.ai>
2024-01-07 15:52:27 +0200

    2023 17 wip 2 g 2

--- python/puzzle_2023_17_g.py
+++ python/puzzle_2023_17_g.py
@@ -1,12 +1,13 @@
 import os
 import sys
+from collections import deque
 from datetime import datetime
 from timeit import default_timer as timer
 
 import networkx as nx
 from colorama import Fore, Back, init
 
-from utils.utils import print_index, print_color, calculate_direction
+from utils.utils import print_index, print_color
 
 print(sys.getrecursionlimit())
 sys.setrecursionlimit(10000)
@@ -29,6 +30,41 @@ def read_file(filename, part=1):
         tiles.append(line)
 
 
+def add_edges(G, x1, y1, x2, y2, rows, cols, visited, target):
+    edges = []
+    key1 = f'{x1},{y1},{x2},{y2}'
+    if key1 in visited:
+        return edges
+    visited.add(key1)
+    vertical = x1 == x2
+    if 0 <= x2 < rows and 0 <= y2 < cols:
+        temp_c = 0
+        for j in range(1, 4):
+            x3 = x2 + j if vertical else x2
+            y3 = y2 + j if not vertical else y2
+            if 0 <= x3 < rows and 0 <= y3 < cols:
+                key2 = f'{x2},{y2},{x3},{y3}'
+                temp_c += int(tiles[x3][y3])
+                G.add_edge(key1, key2, weight=temp_c)
+                if key2.endswith(target):
+                    G.add_edge(key2, target, weight=temp_c)
+                else:
+                    edges.append([x2, y2, x3, y3])
+        temp_c = 0
+        for j in range(1, 4):
+            x3 = x2 - j if vertical else x2
+            y3 = y2 - j if not vertical else y2
+            if 0 <= x3 < rows and 0 <= y3 < cols:
+                key2 = f'{x2},{y2},{x3},{y3}'
+                temp_c += int(tiles[x3][y3])
+                G.add_edge(key1, key2, weight=temp_c)
+                if key2.endswith(target):
+                    G.add_edge(key2, target, weight=temp_c)
+                else:
+                    edges.append([x2, y2, x3, y3])
+    return edges
+
+
 def solve(part=1):
     print_index(tiles, color=Fore.CYAN, ending="")
     rows, cols = len(tiles), len(tiles[0])
@@ -36,73 +72,54 @@ def solve(part=1):
     if part == 1:
         start = [0, 0]
         end = [rows - 1, cols - 1]
-
         source = f'{start[0]},{start[1]}'
         target = f'{end[0]},{end[1]}'
 
-        G = nx.Graph()
-        for x in range(rows):
-            for y in range(cols):
-                key = f"{x},{y}"
+        G = nx.DiGraph()
+        G.add_node(source)
+        G.add_node(target)
 
-                xv_plus = 0
-                xv_minus = 0
-                yv_plus = 0
-                yv_minus = 0
-                for i in range(1, 4):
-                    x2 = x + i
-                    if 0 < x2 < rows:
-                        xv_plus += int(tiles[x2][y])
-                        G.add_edge(key, f"{x2},{y}", weight=xv_plus)
-                    x2 = x - i
-                    if 0 < x2 < rows:
-                        xv_minus += int(tiles[x2][y])
-                        G.add_edge(key, f"{x2},{y}", weight=xv_minus)
-                    y2 = y + i
-                    if 0 < y2 < cols:
-                        yv_plus += int(tiles[x][y2])
-                        G.add_edge(key, f"{x},{y2}", weight=yv_plus)
-                    y2 = y - i
-                    if 0 < y2 < cols:
-                        yv_minus += int(tiles[x][y2])
-                        G.add_edge(key, f"{x},{y2}", weight=yv_minus)
+        edges = deque()
+        x, y = start
+        counters = [0, 0, 0, 0]
+        for i in range(1, 4):
+            for c, node in enumerate([[x + i, y], [x - i, y], [x, y + i], [x, y - i]]):
+                x2, y2 = node
+                if 0 <= x2 < rows and 0 <= y2 < rows:
+                    edges.append([x, y, x2, y2])
+                    counters[c] += int(tiles[x2][y2])
+                    G.add_edge(source, f'{x},{y},{x2},{y2}', weight=counters[c])
 
-        # paths = nx.all_shortest_paths(G, source=source, target=target, weight='weight', method='dijkstra')
-        # paths = nx.all_shortest_paths(G, source=source, target=target, weight=wfunction, method='dijkstra')
+        visited = set()
+        while edges:
+            x1, y1, x2, y2 = edges.popleft()
+            e2 = add_edges(G, x1, y1, x2, y2, rows, cols, visited, target)
+            edges.extend(e2)
+
         path = nx.shortest_path(G, source=source, target=target, weight='weight', method='dijkstra')
 
-        # min_heat = sys.maxsize
-        final_path = []
-        # for path in paths:
+        print(f'path -> {path}')
         pathmap = []
         for i, node in enumerate(path):
-            x, y = node.split(',')
-            x, y = int(x), int(y)
-            if i > 0:
-                x2, y2 = path[i - 1].split(',')
-                x2, y2 = int(x2), int(y2)
-                if x2 != x:
-                    for x3 in range(min(x, x2) + 1, max(x, x2)):
+            if len(node.split(',')) < 4:
+                x, y = node.split(',')
+                x, y = int(x), int(y)
+                if [x, y] not in pathmap:
+                    pathmap.append([x, y])
+                continue
+            x, y, x2, y2 = node.split(',')
+            x, y, x2, y2 = int(x), int(y), int(x2), int(y2)
+            if [x, y] not in pathmap:
+                pathmap.append([x, y])
+            if x2 != x:
+                for x3 in range(min(x, x2), max(x, x2) + 1):
+                    if [x3, y] not in pathmap:
                         pathmap.append([x3, y])
-                elif y2 != y:
-                    for y3 in range(min(y, y2) + 1, max(y, y2)):
+            elif y2 != y:
+                for y3 in range(min(y, y2), max(y, y2) + 1):
+                    if [x, y3] not in pathmap:
                         pathmap.append([x, y3])
-            pathmap.append([x, y])
-
-        # heat_loss = calculate_heat_loss(pathmap)
-        # if heat_loss > min_heat:
-        #     continue
-        # final_paths.append(pathmap)
-
         final_path = pathmap
-
-        # if heat_loss < min_heat:
-        #     final_path = pathmap
-        #     print(f'path length {len(path)}')
-        #     print(f'min heat {heat_loss}')
-        # print_index(tiles, pathmap)
-
-
 
     else:
         #  todo
@@ -110,9 +127,10 @@ def solve(part=1):
         res = max_res
 
     # res = min_heat
+    # final_path = list(set(final_path))
     res = calculate_heat_loss(final_path)
-
     print_index(tiles, final_path)
+    print(final_path)
     print_color(f"---------> final result: {res} <---------", Fore.LIGHTRED_EX, Back.LIGHTYELLOW_EX)
     print_color(f"---------> final result: {final_path} <---------", Fore.LIGHTRED_EX, Back.LIGHTYELLOW_EX)
     return res
@@ -130,46 +148,6 @@ def calculate_heat_loss(path):
             continue
         heat_loss += int(tiles[p[0]][p[1]])
     return heat_loss
-
-
-def find_cases(x, y, previous_x=-1, previous_y=-1, path=[]):
-    all_cases = []
-
-    direction_counters = {'R': 0, 'D': 0, 'L': 0, 'U': 0}
-    last_n = len(path) - 1
-    check_counter = 3
-    for n in range(last_n, last_n - check_counter, -1):
-        if n - 1 < 0:
-            continue
-        p1 = path[n - 1]
-        p2 = path[n]
-        d = calculate_direction(p1[0], p1[1], p2[0], p2[1])
-        direction_counters[d] = direction_counters[d] + 1
-
-    if direction_counters['R'] < check_counter:
-        all_cases.append([x, y + 1])  # go R
-    if direction_counters['U'] < check_counter:
-        all_cases.append([x - 1, y])  # go U
-    if direction_counters['D'] < check_counter:
-        all_cases.append([x + 1, y])  # go D
-    if direction_counters['L'] < check_counter:
-        all_cases.append([x, y - 1])  # go L
-
-    cases = []
-    # print(f'case {x}, {y}, {previous_x}, {previous_y}')
-
-    for case in all_cases:
-        if case[0] < 0 or case[1] < 0 or case[0] >= len(tiles) or case[1] >= len(tiles[0]):
-            continue
-        elif case[0] == previous_x and case[1] == previous_y:
-            continue
-        elif case in path:
-            continue
-        # else:
-        #     find_neighbors(case)
-        cases.append(case)
-
-    return cases
 
 
 def puzzle1(filename):
@@ -197,8 +175,10 @@ if __name__ == '__main__':
     current_time = now.strftime("%H:%M:%S")
     print_color(f"Start Time = {current_time}", Fore.YELLOW)
 
-    puzzle1('../puzzles/2023/17/example.txt')  # result -> 102
-    # puzzle1('../puzzles/2023/17/input.txt')  # result ->
+    # assert puzzle1('../puzzles/2023/17/example.txt') == 102  # result -> 102
+    # puzzle1('../puzzles/2023/17/input.txt')  # result -> 884 too high
+    # puzzle1('../puzzles/2023/17/input.txt')  # result -> 637 too high
+    assert puzzle1('../puzzles/2023/17/input.txt') == 635  # result -> 635 correct
     # puzzle2('../puzzles/2023/17/example.txt')  # result ->
     # puzzle2('../puzzles/2023/17/input.txt')  # result ->
 
