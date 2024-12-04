159afa4a11a6dc4006de514bf431c11e5b40cf30
alexandros.polichronopoulos <alexandros.polichronopoulos@d-one.ai>
2024-12-04 12:17:28 +0200

    2024 day 4 part 2

--- python/2024/puzzle_2024_04.py
+++ python/2024/puzzle_2024_04.py
@@ -5,16 +5,11 @@ from utils.utils import puzzle, read_file, time_and_color
 
 sys.setrecursionlimit(10000)
 
-lines = []
 
-word1 = "XMAS"
-word2 = "SAMX"
-
-
-def check_sequence(line):
+def check_sequence(line, words):
     res = 0
-    res += line.count(word1)
-    res += line.count(word2)
+    for word in words:
+        res += line.count(word)
     return res
 
 
@@ -24,52 +19,67 @@ def solve(part=1, elements=None):
     l = len(elements[0])
     h = len(elements)
 
-    for i in range(0, h):
-        res += check_sequence("".join(elements[i][:]))
+    if part == 1:
+        words = ["XMAS", "SAMX"]
 
-    for j in range(0, l):
-        line = ""
+        # Check horizontal
         for i in range(0, h):
-            line += elements[i][j]
-        res += check_sequence(line)
+            res += check_sequence("".join(elements[i][:]), words)
 
-    # Check diagonals part 1
-    for i in range(0, h):
+        # Check vertical
         for j in range(0, l):
-            if j != 0 and i != 0:
-                continue
+            line = ""
+            for i in range(0, h):
+                line += elements[i][j]
+            res += check_sequence(line, words)
 
-            line1 = elements[i][j]
-            i1, j1 = i, j
+        # Check diagonals part 1
+        for i in range(0, h):
+            for j in range(0, l):
+                if j != 0 and i != 0:
+                    continue
+
+                line1 = elements[i][j]
+                i1, j1 = i, j
+                while 1 == 1:
+                    i1 += 1
+                    j1 += 1
+                    if i1 < 0 or j1 < 0 or i1 >= h or j1 >= l:
+                        break
+                    line1 += elements[i1][j1]
+                res += check_sequence(line1, words)
+
+                line2 = elements[i][j]
+                i2, j2 = i, j
+                while 1 == 1:
+                    i2 -= 1
+                    j2 += 1
+                    if i2 < 0 or j2 < 0 or i2 >= h or j2 >= l:
+                        break
+                    line2 += elements[i2][j2]
+
+                res += check_sequence(line2, words)
+
+        # Check diagonals part 2
+        for j in range(1, l):
+            i3, j3 = h - 1, j
+            line3 = elements[i3][j3]
             while 1 == 1:
-                i1 += 1
-                j1 += 1
-                if i1 < 0 or j1 < 0 or i1 >= h or j1 >= l:
+                i3 -= 1
+                j3 += 1
+                if i3 < 0 or j3 < 0 or i3 >= h or j3 >= l:
                     break
-                line1 += elements[i1][j1]
-            res += check_sequence(line1)
+                line3 += elements[i3][j3]
+            res += check_sequence(line3, words)
 
-            line2 = elements[i][j]
-            i2, j2 = i, j
-            while 1 == 1:
-                i2 -= 1
-                j2 += 1
-                if i2 < 0 or j2 < 0 or i2 >= h or j2 >= l:
-                    break
-                line2 += elements[i2][j2]
-
-            res += check_sequence(line2)
-
-    for j in range(1, l):
-        i3, j3 = h - 1, j
-        line3 = elements[i3][j3]
-        while 1 == 1:
-            i3 -= 1
-            j3 += 1
-            if i3 < 0 or j3 < 0 or i3 >= h or j3 >= l:
-                break
-            line3 += elements[i3][j3]
-        res += check_sequence(line3)
+    else:
+        words = ["MAS", "SAM"]
+        for i in range(0, h - 2):
+            for j in range(0, l - 2):
+                w1 = elements[i][j] + elements[i + 1][j + 1] + elements[i + 2][j + 2]
+                w2 = elements[i + 2][j] + elements[i + 1][j + 1] + elements[i][j + 2]
+                if w1 in words and w2 in words:
+                    res += 1
 
     return res
 
@@ -81,7 +91,7 @@ if __name__ == "__main__":
     assert puzzle("../../puzzles/2024/04/example.txt", read_file, solve, 1) == 18
     assert puzzle("../../puzzles/2024/04/input.txt", read_file, solve, 1) == 2500
 
-    # assert puzzle("../../puzzles/2024/04/example2.txt", read_file, solve, 2) == 0
-    # assert puzzle("../../puzzles/2024/04/input.txt", read_file, solve, 2) == 0
+    assert puzzle("../../puzzles/2024/04/example.txt", read_file, solve, 2) == 9
+    assert puzzle("../../puzzles/2024/04/input.txt", read_file, solve, 2) == 1933
 
     time_and_color(start=False)
