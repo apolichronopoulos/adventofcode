6d1a0691d9cb1c345775b3b755e48e8957e4296b
alexandros.polichronopoulos <alexandros.polichronopoulos@d-one.ai>
2024-12-06 11:54:03 +0200

    2024 day 06 part 2 improvements

--- python/2024/puzzle_2024_06.py
+++ python/2024/puzzle_2024_06.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
-from copy import deepcopy
+from functools import lru_cache
 
-import numpy as np
 from utils.utils import file, print_index, puzzle, read_file, time_and_color
 
 sys.setrecursionlimit(10000)
@@ -34,10 +33,9 @@ def find_path(matrix, start, block):
     path = [start]
     path_with_d = set()
     path_with_d.add((i, j, c))
-
     loop = 0
 
-    while valid_loc(i, j, h, l):
+    while True:
 
         stepi, stepj = moves_forward[c]
         i2, j2 = i + stepi, j + stepj
@@ -53,27 +51,21 @@ def find_path(matrix, start, block):
                 break
             c2 = matrix[i2][j2]
 
-        if (i, j) == (i2, j2):
-            print(f"Found a dead end")
-            loop += 1
+        path.append([i2, j2])
+        new_path = (i2, j2, c)
+        if new_path in path_with_d:
+            loop = 1
             break
-
-        if valid_loc(i2, j2, h, l):
-            path.append([i2, j2])
-            new_path = (i2, j2, c)
-            if new_path in path_with_d:  # or len(path) >= h * l:
-                loop += 1
-                print(f"Found an infinite loop")
-                break
-            path_with_d.add(new_path)
-            i, j = i2, j2
+        path_with_d.add(new_path)
+        i, j = i2, j2
 
     if block:
         matrix[block[0]][block[1]] = "."
 
-    return path, path_with_d, loop
+    return path, loop
 
 
+@lru_cache
 def valid_loc(i, j, h, l):
     return 0 <= i < h and 0 <= j < l
 
@@ -82,8 +74,7 @@ def solve(part=1, elements=None):
     res = 0
 
     matrix = elements
-    np_matrix = np.array(elements)
-    h, l = np_matrix.shape
+    h, l = len(matrix), len(matrix[0])
     print(f"h: {h}, l: {l}")
 
     start = None
@@ -100,14 +91,14 @@ def solve(part=1, elements=None):
                 if matrix[i][j] == ".":
                     blocks.append([i, j])
 
-    for index, block in enumerate(blocks):
-        print(f"block index: {index}")
-        path, path_with_d, loop = find_path(matrix, start, block)
+    for block in blocks:
+        path, loop = find_path(matrix, start, block)
 
         if part == 1:
-            res += len(list(set([f"{i}-{j}" for i, j in path])))
-            print_index(matrix, path)
-        elif part == 2:
+            res += len(list(set([(i, j) for i, j in path])))
+            if debug:
+                print_index(matrix, path)
+        else:
             res += loop
 
     return res
