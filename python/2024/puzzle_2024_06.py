15e9b1d8505ee9426cc990d0faad74e7b4a4cbe5
alexandros.polichronopoulos <alexandros.polichronopoulos@d-one.ai>
2024-12-06 10:28:38 +0200

    2024 day 06 part 2

--- python/2024/puzzle_2024_06.py
+++ python/2024/puzzle_2024_06.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 import sys
+from copy import deepcopy
 
 import numpy as np
 from utils.utils import file, print_index, puzzle, read_file, time_and_color
 
 sys.setrecursionlimit(10000)
-
-matrix = []
-path = []
 
 moves_forward = {
     "^": (-1, 0),
@@ -24,34 +22,21 @@ turn_right = {
 }
 
 
-def valid_loc(i, j, h, l):
-    return 0 <= i < h and 0 <= j < l
+def find_path(matrix, start, block):
+    h, l = len(matrix), len(matrix[0])
 
-
-def solve(part=1, elements=None):
-    res = 0
-
-    matrix = elements
-    path.clear()
-
-    np_matrix = np.array(elements)
-    h, l = np_matrix.shape
-    print(f"h: {h}, l: {l}")
-
-    for i in range(h):
-        if len(path) != 0:
-            break
-        for j in range(l):
-            c = matrix[i][j]
-            if c != "." and c != "#":
-                path.append([i, j])
-                break
-
-    if debug:
-        print(f"part: {part}")
-
-    i, j = path[-1]
+    i, j = start
     c = matrix[i][j]
+
+    if block:
+        matrix[block[0]][block[1]] = "O"
+
+    path = [start]
+    path_with_d = set()
+    path_with_d.add((i, j, c))
+
+    loop = 0
+
     while valid_loc(i, j, h, l):
 
         stepi, stepj = moves_forward[c]
@@ -60,7 +45,7 @@ def solve(part=1, elements=None):
             break
 
         c2 = matrix[i2][j2]
-        while c2 == "#":
+        while c2 in ["#", "O"]:
             c = turn_right[c]
             stepi, stepj = moves_forward[c]
             i2, j2 = i + stepi, j + stepj
@@ -69,18 +54,61 @@ def solve(part=1, elements=None):
             c2 = matrix[i2][j2]
 
         if (i, j) == (i2, j2):
-            print("loop")
+            print(f"Found a dead end")
+            loop += 1
             break
 
         if valid_loc(i2, j2, h, l):
             path.append([i2, j2])
+            new_path = (i2, j2, c)
+            if new_path in path_with_d:  # or len(path) >= h * l:
+                loop += 1
+                print(f"Found an infinite loop")
+                break
+            path_with_d.add(new_path)
             i, j = i2, j2
 
-    if debug:
-        print_index(matrix, path)
+    if block:
+        matrix[block[0]][block[1]] = "."
 
-    if part == 1:
-        res += len(list(set([f"{i}-{j}" for i, j in path])))
+    return path, path_with_d, loop
+
+
+def valid_loc(i, j, h, l):
+    return 0 <= i < h and 0 <= j < l
+
+
+def solve(part=1, elements=None):
+    res = 0
+
+    matrix = elements
+    np_matrix = np.array(elements)
+    h, l = np_matrix.shape
+    print(f"h: {h}, l: {l}")
+
+    start = None
+    for i in range(h):
+        for j in range(l):
+            if matrix[i][j] in ["^", "v", ">", "<"]:
+                start = [i, j]
+                break
+
+    blocks = [[]]
+    if part == 2:
+        for i in range(h):
+            for j in range(l):
+                if matrix[i][j] == ".":
+                    blocks.append([i, j])
+
+    for index, block in enumerate(blocks):
+        print(f"block index: {index}")
+        path, path_with_d, loop = find_path(matrix, start, block)
+
+        if part == 1:
+            res += len(list(set([f"{i}-{j}" for i, j in path])))
+            print_index(matrix, path)
+        elif part == 2:
+            res += loop
 
     return res
 
@@ -91,7 +119,7 @@ if __name__ == "__main__":
 
     assert puzzle(file("/2024/06/example.txt"), read_file, solve, 1) == 41
     assert puzzle(file("/2024/06/input.txt"), read_file, solve, 1) == 4826
-    # assert puzzle(file("/2024/06/example.txt"), read_file, solve, 2) == 6
-    # assert puzzle(file("/2024/06/input.txt"), read_file, solve, 2) == 0
+    assert puzzle(file("/2024/06/example.txt"), read_file, solve, 2) == 6
+    assert puzzle(file("/2024/06/input.txt"), read_file, solve, 2) == 1721
 
     time_and_color(start=False)
