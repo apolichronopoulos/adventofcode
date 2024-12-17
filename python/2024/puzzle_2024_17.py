794ce41ba08e710e9f49d8c4bf4a4d63450e190c
alexandros.polichronopoulos <alexandros.polichronopoulos@d-one.ai>
2024-12-17 10:16:01 +0200

    2024 day 17 part 2 (wip)

--- python/2024/puzzle_2024_17.py
+++ python/2024/puzzle_2024_17.py
@@ -2,7 +2,7 @@
 import math
 import sys
 
-from utils.utils import aoc_submit, file, print_index, puzzle, time_and_color, valid_loc
+from utils.utils import aoc_submit, file, puzzle, time_and_color
 
 sys.setrecursionlimit(20000)
 
@@ -75,26 +75,65 @@ def calculate(opcode, operand):
     return res
 
 
-def solve(part=1):
+def run_program(a, b, c, expected=[]):
+    results.clear()
+    registers.clear()
+    registers.extend([a, b, c])
     global intr_pointer
+    intr_pointer = 0
     while intr_pointer < len(opcodes) - 1:
         opcode, operand = opcodes[intr_pointer], opcodes[intr_pointer + 1]
         calculate(opcode, operand)
+        for i in range(min(len(expected), len(results))):
+            if expected[i] != results[i]:
+                return None
+
     return ",".join([str(r) for r in results])
+
+
+def solve(part=1):
+    global intr_pointer
+    a, b, c = registers
+    res = run_program(a, b, c)
+    if part == 1:
+        return res
+
+    expected = opcodes
+    expected_res = ",".join([str(s) for s in opcodes])
+
+    if debug:
+        print(res)
+        print(expected_res)
+
+    min_a = -1
+    while True:
+        min_a += 1
+        if min_a == a:
+            continue
+        res2 = run_program(min_a, b, c, expected)
+        if res2 and res2 == expected_res:
+            if debug:
+                print(res2)
+            break
+
+    return min_a
 
 
 if __name__ == "__main__":
     time_and_color(start=True)
     debug = True
-    submit = True  # be careful
+    submit = False  # be careful
 
-    assert (
-        puzzle(file("/2024/17/example.txt"), read_file, solve, 1)
-        == "4,6,3,5,6,3,5,2,1,0"
-    )
-    answer1 = puzzle(file("/2024/17/input.txt"), read_file, solve, 1)
-    assert answer1 == "6,7,5,2,1,3,5,1,7"
-    if submit:
-        aoc_submit("2024", "17", 1, answer1)
+    # assert (puzzle(file("/2024/17/example.txt"), read_file, solve, 1) == "4,6,3,5,6,3,5,2,1,0")
+    # answer1 = puzzle(file("/2024/17/input.txt"), read_file, solve, 1)
+    # assert answer1 == "6,7,5,2,1,3,5,1,7"
+    # if submit:
+    #     aoc_submit("2024", "17", 1, answer1)
+
+    # assert (puzzle(file("/2024/17/example2.txt"), read_file, solve, 2) == 117440)
+    answer2 = puzzle(file("/2024/17/input.txt"), read_file, solve, 2)  # takes > 6 mins
+    assert answer2 != 117440
+    # if submit:
+    #     aoc_submit("2024", "17", 2, answer2)
 
     time_and_color(start=False)
