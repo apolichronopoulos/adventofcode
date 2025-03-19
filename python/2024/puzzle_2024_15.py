83d3d9f0cc891501783d7a7586970104219b1706
alexandros.polichronopoulos <alexandros.polichronopoulos@d-one.ai>
2025-03-19 16:38:40 +0200

    2024 day 15 part 2 improvements

--- python/2024/puzzle_2024_15.py
+++ python/2024/puzzle_2024_15.py
@@ -67,42 +67,48 @@ def move_item(x, y, m, h, l, box=False):
         return True
 
 
-def move_robot(x, y, m, h, l, part=1):
+def move_robot(x, y, m, h, l):
     x1 = x + moves_forward[m][0]
     y1 = y + moves_forward[m][1]
     if not valid_loc(x1, y1, h, l) or (x1, y1) in walls:
         return False
 
+    elements_bu = [[e for e in row] for row in elements]
+    boxes_bu = set(b for b in boxes)
     moved = True
     if (x1, y1) in boxes:
         moved = move_box(x1, y1, m, h, l)
-
     if moved:
         robot.clear()
         robot.add((x1, y1))
+        elements[x][y] = "."
+        elements[x1][y1] = "@"
+    else:
+        elements.clear()
+        for row in elements_bu:
+            elements.append(row)
+        boxes.clear()
+        for box in boxes_bu:
+            boxes.add(box)
 
     return moved
 
 
 def move_box(x, y, m, h, l):
     c = elements[x][y]
-    dy = 1 if c == "[" else -1
     if c == "[":
         xl, yl = x, y
-        xr, yr = x, y + dy
-        x1 = x + moves_forward[m][0]
-        y1 = y + moves_forward[m][1]
-        x2 = x + moves_forward[m][0]
-        y2 = y + 1 + moves_forward[m][1]
+        xr, yr = x, y + 1
     elif c == "]":
-        xl, yl = x, y + dy
+        xl, yl = x, y - 1
         xr, yr = x, y
-        x1 = x + moves_forward[m][0]
-        y1 = y - 1 + moves_forward[m][1]
-        x2 = x + moves_forward[m][0]
-        y2 = y + moves_forward[m][1]
     else:
         raise "error"
+
+    x1 = xl + moves_forward[m][0]
+    y1 = yl + moves_forward[m][1]
+    x2 = xr + moves_forward[m][0]
+    y2 = yr + moves_forward[m][1]
 
     if (not valid_loc(x1, y1, h, l)) or (x1, y1) in walls:
         return False
@@ -117,19 +123,10 @@ def move_box(x, y, m, h, l):
     if m == ">" and (x2, y2) in boxes:
         moved1 = move_box(x2, y2, m, h, l)
     if m in ["^", "v"]:
-        i, j = moves_forward[m]
         if (x1, y1) in boxes:
             moved1 = move_box(x1, y1, m, h, l)
-        if (x2, y2) in boxes:
+        if moved1 and (x2, y2) in boxes:
             moved2 = move_box(x2, y2, m, h, l)
-
-        c1 = elements[x1][y1]
-        c2 = elements[x2][y2]
-        if c1 != "." and c2 != ".":
-            if moved1 and not moved2:
-                move_back(x1 + i, y1 + j, m)
-            if not moved1 and moved2:
-                move_back(x2 + i, y2 + j, m)
 
     if moved1 and moved2:
         if (xl, yl) in boxes:
@@ -147,50 +144,14 @@ def move_box(x, y, m, h, l):
     return moved1 and moved2
 
 
-def move_back(x, y, m):
-    c = elements[x][y]
-    if c == "[":
-        x1, y1 = x, y
-        x2, y2 = x, y + 1
-    else:
-        x1, y1 = x, y - 1
-        x2, y2 = x, y
-
-    i, j = moves_forward[m]
-    i, j = -i, -j
-
-    if (x1, y1) not in boxes or (x2, y2) not in boxes:
-        return
-
-    boxes.remove((x1, y1))
-    boxes.remove((x2, y2))
-    boxes.add((x1 + i, y1 + j))
-    boxes.add((x2 + i, y2 + j))
-
-    elements[x1][y1] = "."
-    elements[x2][y2] = "."
-    elements[x1 + i][y1 + j] = "["
-    elements[x2 + i][y2 + j] = "]"
-
-
-def print_grid(h, l):
-    for i in range(h):
-        for j in range(l):
-            if (i, j) in boxes:
-                a = 1
-            elif (i, j) in walls:
-                elements[i][j] = "#"
-            elif (i, j) in robot:
-                elements[i][j] = "@"
-            else:
-                elements[i][j] = "."
+def print_grid():
     print_index(
         elements,
         counts=[[i, j] for i, j in boxes],
         results=[[i, j] for i, j in walls],
         tuples=robot,
         tuple_char="",
-        ending=" ",
+        ending="",
     )
 
 
@@ -222,18 +183,17 @@ def solve(part=1):
     robot.add(start)
 
     if debug:
-        print_grid(h, l)
+        print_grid()
 
-    for m in moves:
+    for e, m in enumerate(moves):
         r = robot.pop()
         robot.add(r)
         if part == 1:
             moved = move_item(r[0], r[1], m, h, l, box=False)
         else:
             moved = move_robot(r[0], r[1], m, h, l)
-            if debug and moved:
-                print(f"\nMove: {m}")
-                print_grid(h, l)
+            if debug:
+                print_grid()
 
     if part == 1:
         for b in boxes:
@@ -267,24 +227,20 @@ def solve(part=1):
 if __name__ == "__main__":
     time_and_color(start=True)
     debug = False
-    submit = True  # be careful
+    submit = False  # be careful
 
-    # assert puzzle(file("/2024/15/example.txt"), read_file, solve, 1) == 2028
-    # assert puzzle(file("/2024/15/example2.txt"), read_file, solve, 1) == 10092
-    # answer1 = puzzle(file("/2024/15/input.txt"), read_file, solve, 1)
-    # assert answer1 == 1492518
-    # if submit:
-    #     aoc_submit("2024", "15", 1, answer1)
+    assert puzzle(file("/2024/15/example.txt"), read_file, solve, 1) == 2028
+    assert puzzle(file("/2024/15/example2.txt"), read_file, solve, 1) == 10092
+    answer1 = puzzle(file("/2024/15/input.txt"), read_file, solve, 1)
+    assert answer1 == 1492518
+    if submit:
+        aoc_submit("2024", "15", 1, answer1)
 
-    # assert puzzle(file("/2024/15/example3.txt"), read_file, solve, 2) == 618
-    # debug = True
-    # assert puzzle(file("/2024/15/example2.txt"), read_file, solve, 2) == 9021
-    # debug = False
+    assert puzzle(file("/2024/15/example3.txt"), read_file, solve, 2) == 618
+    assert puzzle(file("/2024/15/example2.txt"), read_file, solve, 2) == 9021
     answer2 = puzzle(file("/2024/15/input.txt"), read_file, solve, 2)
     assert answer2 != 1500827
-    assert answer2 != 1501117
-    assert answer2 != 722919
-    assert answer2 > 1500827
+    assert answer2 == 1512860
     if submit:
         aoc_submit("2024", "15", 2, answer2)
 
