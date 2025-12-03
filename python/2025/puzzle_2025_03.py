05820ae76082e639ff939245d9555d7e35905ea3
Alexandros Polichronopoulos <alexp.private@gmail.com>
2025-12-03 10:08:20 +0200

    2025 day 03 part 2

--- python/2025/puzzle_2025_03.py
+++ python/2025/puzzle_2025_03.py
@@ -28,28 +28,37 @@ def solve(part=1):
         bank = batteries[i]
         if debug:
             print(f"bank: {bank}")
-        if part == 1:
-            max1, max1_i = -1, -1
-            for n in range(len(bank) - 1):
-                if bank[n] > max1:
-                    max1, max1_i = bank[n], n
 
-            max2, max2_i = -1, -1
-            for n in range(max1_i + 1, len(bank)):
-                if bank[n] > max2:
-                    max2, max2_i = bank[n], n
+        size = 2 if part == 1 else 12
+        position = 0
+        total = ""
 
-            if debug:
-                print(f"max1: {max1}, max2: {max2}")
-            res += int(str(max1) + str(max2))
+        for remaining in range(size, 0, -1):
+            x, i = find_max(bank, position, remaining)
+
+            total += str(x)
+            position = i + 1
+
+        if debug:
+            print(f"total: {total}")
+
+        res += int(total)
 
     return res
 
 
+def find_max(bank, position, remaining):
+    x, i = -1, -1
+    for n in range(position, len(bank) - remaining + 1):
+        if bank[n] > x:
+            x, i = bank[n], n
+    return x, i
+
+
 if __name__ == "__main__":
     time_and_color(start=True)
-    submit = True  # be careful
-    debug = True
+    submit = False  # be careful
+    debug = False
 
     assert puzzle(file("/2025/03/example.txt"), read_file, solve, 1) == 357
     answer1 = puzzle(file("/2025/03/input.txt"), read_file, solve, 1)
@@ -58,15 +67,12 @@ if __name__ == "__main__":
     if submit:
         aoc_submit("2025", "03", 1, answer1)
 
-    # if debug:
-    #     test_split_text()
+    assert puzzle(file("/2025/03/example.txt"), read_file, solve, 2) == 3121910778619
 
-    # assert puzzle(file("/2025/03/example.txt"), read_file, solve, 2) == -1
+    answer2 = puzzle(file("/2025/03/input.txt"), read_file, solve, 2)
+    assert answer2 == 172681562473501
 
-    # answer2 = puzzle(file("/2025/03/input.txt"), read_file, solve, 2)
-    # assert answer2 == -1
-
-    # if submit:
-    #     aoc_submit("2025", "03", 2, answer2)
+    if submit:
+        aoc_submit("2025", "03", 2, answer2)
 
     time_and_color(start=False)
