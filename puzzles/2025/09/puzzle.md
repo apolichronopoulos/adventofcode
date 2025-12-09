f7c0f68ec5a56253b07271bd1c749996ccf5ed5e
Alexandros Polichronopoulos <alexp.private@gmail.com>
2025-12-10 00:19:34 +0200

    2025 day 09 part 2

--- puzzles/2025/09/puzzle.md
+++ puzzles/2025/09/puzzle.md
@@ -100,8 +100,6 @@ Using two red tiles as opposite corners, *what is the largest area of any rectan
 
 Your puzzle answer was `4777409595`.
 
-The first half of this puzzle is complete! It provides one gold star: \*
-
 \--- Part Two ---
 ----------
 
@@ -190,8 +188,12 @@ The largest rectangle you can make in this example using only red and green tile
 
 Using two red tiles as opposite corners, *what is the largest area of any rectangle you can make using only red and green tiles?*
 
-Answer:
+Your puzzle answer was `1473551379`.
 
-Although it hasn't changed, you can still [get your puzzle input](9/input).
+Both parts of this puzzle are complete! They provide two gold stars: \*\*
 
-You can also [Shareon [Bluesky](https://bsky.app/intent/compose?text=I%27ve+completed+Part+One+of+%22Movie+Theater%22+%2D+Day+9+%2D+Advent+of+Code+2025+%23AdventOfCode+https%3A%2F%2Fadventofcode%2Ecom%2F2025%2Fday%2F9) [Twitter](https://twitter.com/intent/tweet?text=I%27ve+completed+Part+One+of+%22Movie+Theater%22+%2D+Day+9+%2D+Advent+of+Code+2025&url=https%3A%2F%2Fadventofcode%2Ecom%2F2025%2Fday%2F9&related=ericwastl&hashtags=AdventOfCode) [Mastodon](javascript:void(0);)] this puzzle.
+At this point, you should [return to your Advent calendar](/2025) and try another puzzle.
+
+If you still want to see it, you can [get your puzzle input](9/input).
+
+You can also [Shareon [Bluesky](https://bsky.app/intent/compose?text=I%27ve+completed+%22Movie+Theater%22+%2D+Day+9+%2D+Advent+of+Code+2025+%23AdventOfCode+https%3A%2F%2Fadventofcode%2Ecom%2F2025%2Fday%2F9) [Twitter](https://twitter.com/intent/tweet?text=I%27ve+completed+%22Movie+Theater%22+%2D+Day+9+%2D+Advent+of+Code+2025&url=https%3A%2F%2Fadventofcode%2Ecom%2F2025%2Fday%2F9&related=ericwastl&hashtags=AdventOfCode) [Mastodon](javascript:void(0);)] this puzzle.
--- python/2025/puzzle_2025_09.py
+++ python/2025/puzzle_2025_09.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 import sys
 
+from shapely.geometry import Polygon, box
 from utils.utils import (
     aoc_submit,
     calculate_area,
     custom_args,
-    euclidean_distance,
     file,
+    get_combinations,
+    print_index,
     puzzle,
     time_and_color,
 )
@@ -16,35 +18,121 @@ sys.setrecursionlimit(10000)
 
 grid = []
 reds = []
+greens = []
 
 
 def read_file(filename, part=1):
     f = open(filename, "r")
     grid.clear()
     reds.clear()
+    greens.clear()
     for i, line in enumerate(f):
         line = line.strip()
         if line == "":
             continue
-        x, y = line.split(",")
+        y, x = line.split(",")
         reds.append((int(x), int(y)))
+
+    if debug:
+        max_i = max(x for x, y in reds) + 2
+        max_j = max(y for x, y in reds) + 3
+
+        for i in range(max_i):
+            row = []
+            for j in range(max_j):
+                row.append("#" if (i, j) in reds else ".")
+            grid.append(row)
 
 
 def solve(part=1):
     res = 0
 
-    sizes = {}
-    for r1 in reds:
-        for r2 in reds:
-            if r1 == r2:
-                continue
+    if debug:
+        print_index(grid, reds)
+        print("--------------------")
 
-            sizes[(r1, r2)] = calculate_area(r1, r2)
+    if part == 1:
+        res += find_max_area(reds)
+    else:
+        if debug:
+            greens_x = set()
+            greens_y = set()
 
-    sizes = sorted(sizes.items(), key=lambda x: -x[1])
-    res += sizes[0][1]
+            for r1, r2 in get_combinations(reds):
+                x1, y1 = r1
+                x2, y2 = r2
+                if x1 == x2:
+                    greens_x.add((x1, min(y1, y2), max(y1, y2)))
+                if y1 == y2:
+                    greens_y.add((y1, min(x1, x2), max(x1, x2)))
+
+            for x, y1, y2 in greens_x:
+                for y in range(y1 + 1, y2):
+                    g = (x, y)
+                    if g not in reds:
+                        greens.append(g)
+            for y, x1, x2 in greens_y:
+                for x in range(x1 + 1, x2):
+                    g = (x, y)
+                    if g not in reds:
+                        greens.append(g)
+
+            print(f"Initialized greens: {len(greens)}")
+            print_index(grid, reds, tuples=greens, tuple_char="X")
+            print("--------------------")
+            print(f"Running final area check")
+
+        wall = Polygon(reds)
+
+        res = find_max_area2(reds, wall)
 
     return res
+
+
+def find_max_area(points, extra_points=[]):
+    sizes = {}
+
+    for p1, p2 in get_combinations(points):
+        sizes[(p1, p2)] = calculate_area(p1, p2)
+
+    sizes = sorted(sizes.items(), key=lambda x: -x[1])
+
+    if extra_points:
+        for s in sizes:
+            p1, p2 = s[0]
+            size = s[1]
+            valid = True
+            for x in range(min(p1[0], p2[0]), max(p1[0], p2[0]) + 1):
+                for y in range(min(p1[1], p2[1]), max(p1[1], p2[1]) + 1):
+                    if (x, y) not in extra_points:
+                        valid = False
+                        break
+                if not valid:
+                    break
+            if valid:
+                return size
+
+    return sizes[0][1]
+
+
+def find_max_area2(points, wall):
+    sizes = {}
+    for p1, p2 in get_combinations(points):
+        sizes[(p1, p2)] = calculate_area(p1, p2)
+
+    sizes = sorted(sizes.items(), key=lambda x: -x[1])
+    for s in sizes:
+        p1, p2 = s[0]
+        size = s[1]
+
+        rect_min_x, rect_min_y = min(p1[0], p2[0]), min(p1[1], p2[1])
+        rect_max_x, rect_max_y = max(p1[0], p2[0]), max(p1[1], p2[1])
+        rectangle = box(rect_min_x, rect_min_y, rect_max_x, rect_max_y)
+
+        if rectangle.within(wall):  # True if inside or touching the boundary
+            return size
+
+    return -1
 
 
 if __name__ == "__main__":
@@ -58,11 +146,11 @@ if __name__ == "__main__":
     if submit:
         aoc_submit("2025", "09", 1, answer1)
 
-    # assert puzzle(file("/2025/09/example.txt"), read_file, solve, 2) == -1
-    # answer2 = puzzle(file("/2025/09/input.txt"), read_file, solve, 2)
-    # assert answer2 == -1
-    #
-    # if submit:
-    #     aoc_submit("2025", "09", 2, answer2)
+    assert puzzle(file("/2025/09/example.txt"), read_file, solve, 2) == 24
+    answer2 = puzzle(file("/2025/09/input.txt"), read_file, solve, 2)
+    assert answer2 == 1473551379
+
+    if submit:
+        aoc_submit("2025", "09", 2, answer2)
 
     time_and_color(start=False)
